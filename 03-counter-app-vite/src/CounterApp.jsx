0c5244f9247aa531f0f85037b9d492fe9d917c3d
Andrés Pausin <candrespausin2001@gmail.com>
2025-06-02 21:19:24 -0400

    add: the useState in react

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
+// La idea de manejar eventos fuera del componente es que no se re-renderice el componente al hacer click en el botón
+    const getEvent = (event) => {
+        console.log(event);
+    }
+
 export const CounterApp = ({ value }) => {
+    const [counter, setCount] = useState(value);
+
+    // Funcion que se ejecuta al hacer click en el botón
+    // Modifica el estado del componente, el cual se declaro arriba con el valor inicial de value
+    const handleClick = () => {
+        setCount(counter+1)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ parseFloat(value) ? `Este es un numero: ${value}` : "Debes adjuntar un numero como prop"}</h2>
+            <h2>{ `Contador: ${counter}` }</h2>
+
+            <button onClick={handleClick}>
+                +1
+            </button>
         </>
     )
 }
