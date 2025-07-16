7a841834bd9b051988694c013ae5d17f0b741d43
Andrés Pausín <111588426+andrespausin@users.noreply.github.com>
2025-07-16 21:52:26 +0000

    add: modifying the principal state

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['dragon ball', 'one punch man'])
+
+    const onAddCategory = () => {
+        setCategories([...categories, 'Hola mundo'])
+    }
+
     return (
         <>
+            {/* Titulo */}
             <h1>Gif Expert App</h1>
+
+            {/* Input */}
+
+            {/* .Listado de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                {
+                    categories.map((category, index) => {
+                        return <li key={index}>{category}</li>
+                    })
+                }
+            </ol>
+                {/* Gif Item */}
         </>
     )
 }
