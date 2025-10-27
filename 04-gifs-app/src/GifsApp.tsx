71b5a1e607c674914bc2d0c8e76e498a72c64ec8
Andrés Pausin <candrespausin2001@gmail.com>
2025-10-27 19:05:53 +0100

    add: first data modification from gifs app

---
+++ 04-gifs-app/src/GifsApp.tsx
@@ -0,0 +1,44 @@
+import { mockGifs } from "./mock-data/data"
+
+export const GifsApp = () => {
+    return (
+        <>
+            {/* Header */}
+            <div className="content-center">
+                <h1>Buscador de Gifs</h1>
+                <p>Descubre y comparte el gif perfecto</p>
+            </div>
+
+            {/* Search Bar */}
+            <div className="search-container">
+                <input type="text" placeholder="Buscar gifs" />
+                <button>Buscar</button>
+            </div>
+
+            {/* Busquedas previas */}
+            <div className="previous-searches">
+                <h2>Busquedas anteriores</h2>
+                <ul className="previous-searches-list">
+                    <li>Gatos</li>
+                    <li>Perros</li>
+                    <li>Memes</li>
+                </ul>
+            </div>
+
+            {/* Gifs */}
+            <div className="gifs-container">
+                {
+                    mockGifs.map(gif => (
+                        <div key={gif.id} className="gif-card">
+                            <img src={gif.url} alt={gif.title} />
+                            <h3>{gif.title}</h3>
+                            <p>
+                                {gif.width} x {gif.height} (1.5Mb)
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
