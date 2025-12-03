6b56c468b7ba4d76edc7e0ec918ced79aa031079
Andres Pausin <candrespausin2001@gmail.com>
2025-12-03 10:48:28 +0100

    add: created a traffic light component with useEffect

--- 05-hooks-app/src/01-useState/TrafficLight.tsx
+++ 05-hooks-app/src/01-useState/TrafficLight.tsx
@@ -1,24 +1,45 @@
-export const TrafficLight = () => {
+import { useState } from "react";
 
+const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse'
+}
+
+// En caso de agregar colores, no se debe modificar manualmente
+type TrafficColor = keyof typeof colors
+
+export const TrafficLight = () => {
+    const [light, setLight] = useState<TrafficColor>("red");
+
+    const handleColorChange = (color: TrafficColor) => {
+        setLight((prev) => {
+            console.log({ prev })
+            return color;
+        })
+    }
 
     return (
-        <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+        <div className="min-h-screen bg-linear-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
             <div className="flex flex-col items-center space-y-8">
-                <div className="w-32 h-32 bg-red-500 rounded-full"></div>
-                <div className="w-32 h-32 bg-yellow-500 rounded-full"></div>
-                <div className="w-32 h-32 bg-green-500 rounded-full"></div>
+                <div className={`w-32 h-32 ${light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
 
                 {/* Botón para cambiar el estado de la luz */}
                 <div className="flex gap-2">
                     <button
-                        className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer">
+                        className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                        onClick={() => handleColorChange('red')}>
                         Rojo
                     </button>
                     <button
+                        onClick={() => handleColorChange("yellow")}
                         className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer">
                         Amarillo
                     </button>
                     <button
+                        onClick={() => handleColorChange('green')}
                         className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer">
                         Verde
                     </button>
