85e3b6317c3736743575d27610f9e082b83baa7c
Andres Pausin <candrespausin2001@gmail.com>
2025-12-03 12:20:22 +0100

    add: created another example when ussing hooks in the pokemon app

--- 05-hooks-app/src/main.tsx
+++ 05-hooks-app/src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
-import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+// import { TrafficLightWithHook } from './03-withHook/TrafficLightWithHook'
+import { PokemonPage } from './04-examples/PokemonPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
-    <TrafficLightWithEffect />
+    {/* <TrafficLightWithEffect /> */}
+    {/* <TrafficLightWithHook /> */}
+    <PokemonPage />
   </StrictMode>,
 )
