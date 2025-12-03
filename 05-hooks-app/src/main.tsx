be4a7ba6a99f767a71b35de51e5e9bb4e1db52f4
Andres Pausin <candrespausin2001@gmail.com>
2025-12-03 16:59:44 +0100

    installed: shadcnUI

--- 05-hooks-app/src/main.tsx
+++ 05-hooks-app/src/main.tsx
@@ -6,7 +6,8 @@ import './index.css'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './03-withHook/TrafficLightWithHook'
 // import { PokemonPage } from './04-examples/PokemonPage'
-import { FocusScreen } from './05-useRef/FocusScreen'
+// import { FocusScreen } from './05-useRef/FocusScreen'
+import { TasksApp } from './06-useReducer/TaskApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 )
