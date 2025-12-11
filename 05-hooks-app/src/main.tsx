e583d5065f68991b2549012c162437ec53b4d253
Andres Pausin <candrespausin2001@gmail.com>
2025-12-11 17:39:06 +0100

    edit: completed the task by making it work

--- 05-hooks-app/src/main.tsx
+++ 05-hooks-app/src/main.tsx
@@ -8,6 +8,7 @@ import './index.css'
 // import { PokemonPage } from './04-examples/PokemonPage'
 // import { FocusScreen } from './05-useRef/FocusScreen'
 import { TasksApp } from './06-useReducer/TaskApp'
+import { ScrambleWords } from './06-useReducer/ScrambledWords'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 )
