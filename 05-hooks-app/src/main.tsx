2c646a4b7787a256b807f533e8680bc7d132dd72
Cesar Pausin WSL <candrespausin2001@gmail.com>
2025-12-17 19:33:41 +0100

    added: Implemented useTransition and useOptimistic

--- 05-hooks-app/src/main.tsx
+++ 05-hooks-app/src/main.tsx
@@ -7,8 +7,9 @@ import './index.css'
 // import { TrafficLightWithHook } from './03-withHook/TrafficLightWithHook'
 // import { PokemonPage } from './04-examples/PokemonPage'
 // import { FocusScreen } from './05-useRef/FocusScreen'
-import { TasksApp } from './06-useReducer/TaskApp'
-import { ScrambleWords } from './06-useReducer/ScrambledWords'
+// import { TasksApp } from './06-useReducer/TaskApp'
+// import { ScrambleWords } from './06-useReducer/ScrambledWords'
+import { InstagramApp } from './07-useOptimistic/InstagramApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
     {/* <TasksApp /> */}
-    <ScrambleWords />
+    {/* <ScrambleWords /> */}
+    <InstagramApp />
   </StrictMode>,
 )
